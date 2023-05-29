de9004ac8bd9df0319ab6fb8521a3e8ae12f0b66
yehuangyuan <ye.huangyuan@darcytech.com>
2023-05-29 17:11:01 +0800

    第四次提交

--- userServer/startup.sh
+++ userServer/startup.sh
@@ -2,7 +2,7 @@
 
 nohup java -server -Xmx50m -Xms250m -jar ./target/userServer-1.0-SNAPSHOT.jar  --spring.cloud.bootstrap.location=config --spring.profiles.active=release --server.port=8092 & 2>1
 
-#第三次
+#第四次
 
 #nohup java -server -Xmx500m -Xms250m -jar /Users/linglong/Documents/code/fishCloud/userServer/target/userServer-1.0-SNAPSHOT.jar --server.port=8092 >> start.out &
 
